75cc5e192771d15d41a414b080cad88d53905470
christiaanpauw <christiaan.pauw@nova.org.za>
2021-07-21 14:11:10 +0200

    Lee raamwerk

---
+++ R/meanVehicleWeight.R
@@ -0,0 +1,12 @@
+#' @title meanVehicleWeight
+#' @description  Caclulate average weight of all vehicles traveling the road for use in pavedRoadDustEmissions
+#' @concept It is important to note that Equation 1 calls for the average weight of all vehicles traveling the road.
+#' For example, if 99 percent of traffic on the road are 2 ton cars/trucks while the remaining 1 percent consists of
+#' 20 ton trucks, then the mean weight "W" is 2.2 tons. More specifically, Equation 1 is not intended to be used to
+#' calculate a separate emission factor for each vehicle weight class. Instead, only one emission factor should be
+#' calculated to represent the "fleet" average weight of all vehicles traveling the road.
+
+
+meanVehicleWeight <- function(){
+
+}

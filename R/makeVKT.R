3db334329be1f0a0f35f1b74a452753aaf6284ae
christiaanpauw <christiaan.pauw@nova.org.za>
2021-07-21 14:11:25 +0200

    makeVKT eerste commit

---
+++ R/makeVKT.R
@@ -0,0 +1,11 @@
+#' @title makeVKT
+#' @description Calculate the vehicle kilometer travelled
+#' @param n Integer. Count per hour.
+#' @param d Numeric. Distance in kilometer
+
+makeVKT <- function(n = NULL, d = NULL){
+  if (is.null(n) | is.null(d)) stop("n and d cannot be NULL")
+  res <- n * d
+  units(res) <- as_units("km")
+  res
+}

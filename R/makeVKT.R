985bf2f05615c00680dde3901ff007a784b7cfab
christiaanpauw <christiaan.pauw@nova.org.za>
2021-09-12 07:56:52 +0200

    Export

--- R/makeVKT.R
+++ R/makeVKT.R
@@ -2,6 +2,7 @@
 #' @description Calculate the vehicle kilometer travelled
 #' @param n Integer. Count per hour.
 #' @param d Numeric. Distance in kilometer
+#' @export
 
 makeVKT <- function(n = NULL, d = NULL){
   if (is.null(n) | is.null(d)) stop("n and d cannot be NULL")
--- R/makeVehicleDistanceLookup.R
+++ R/makeVehicleDistanceLookup.R
@@ -1,6 +1,7 @@
 #' @title makeVehicleDistanceLookup
 #' @description Create a lookup table for the values of K in EPA 42 Ch 13 Section 13.2.1.3
 #' @references https://www3.epa.gov/ttn/chief/old/ap42/ch13/s021/final/c13s02-1_2002.pdf
+#' @export
 #' @import dplyr
 #' @import magrittr
 
